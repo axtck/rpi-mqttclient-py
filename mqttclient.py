c078e6df62226676d802867ac2aa9692dcde468c
rpi-bo-b <alexandertack@hotmail.be>
2020-12-01 14:55:11 +0100

    Added basic connection, subscription

--- mqttclient.py
+++ mqttclient.py
@@ -1,24 +1,54 @@
 import paho.mqtt.client as mqtt
+import time
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("$SYS/#")
+# On connent callback. 
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected with result code {str(rc)}')
 
-# The callback for when a PUBLISH message is received from the server.
+# On message callback.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(f'Message\nTopic: {msg.topic}:\nPayload: {str(msg.payload)}.')
 
+# On publish callback.
+def on_publish(client, obj, mid):
+    print(f'Mid: {str(mid)}')
+
+# On subscribe callback.
+def on_subscribe(client, obj, mid, grated_qos):
+    print(f'Subbed: {str(obj)}')
+
+
+# Create client.
 client = mqtt.Client()
+
+# Link callbacks.
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
 
-mqtt_host = "farmer.cloudmqtt.com"
-mqtt_port = 34511
-mqtt_user = "itiwppsz"
-mqtt_password = "BkRYsnNyy_tk"
+# Personal client data.
+mqtt_host = 'farmer.cloudmqtt.com'
+mqtt_port = 14511
+mqtt_user = 'itiwppsz'
+mqtt_password = 'BkRYsnNyy_tk'
 
-client.connect("farmer.cloudmqtt.com", 34511, 60)
+# Topic.
+topic = '/io/ion'
 
+# Set credentials.
+client.username_pw_set(mqtt_user, mqtt_password)
 
-client.loop_forever()
+# Connect.
+client.connect(mqtt_host, mqtt_port, 60)
+
+# Subscribe.
+client.subscribe(topic)
+
+# client.loop_forever()
+
+# Continue the network loop, exit when an error occurs
+rc = 0
+while rc == 0:
+    rc = client.loop()
+print(f'rc: {str(rc)}')
