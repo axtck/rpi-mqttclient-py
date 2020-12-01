64b824854cc4aabf16680c7d062ac3ccba4d7261
rpi-bo-b <alexandertack@hotmail.be>
2020-12-01 17:02:36 +0100

    Working read / write

--- mqttclient.py
+++ mqttclient.py
@@ -1,13 +1,25 @@
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
 import time
+import json
 
 # On connent callback. 
 def on_connect(client, userdata, flags, rc):
-    print(f'Connected with result code {str(rc)}.\n')
+    print(f'On connect, result code: {str(rc)}.\n')
 
 # On message callback.
 def on_message(client, userdata, msg):
-    print(f'Message\nTopic: {msg.topic}\nPayload: {str(msg.payload)}.\n')
+    # Parse json to Python dict.
+    command = json.loads(msg.payload)
+
+    # Get pin and status value.
+    pin = int(command['pin']) 
+    status = command['status']
+
+    # Call write_gpio to write pin.
+    write_gpio(pin, status)
+
+    print(f'On message\nTopic: {msg.topic}\nPayload: {str(msg.payload)}\n')
 
 # On publish callback.
 def on_publish(client, obj, mid):
@@ -34,7 +46,8 @@ mqtt_user = 'itiwppsz'
 mqtt_password = 'BkRYsnNyy_tk'
 
 # Topic.
-topic = '/io/ion'
+input_topic = 'io/in'
+output_topic = 'io/out'
 
 # Set credentials.
 client.username_pw_set(mqtt_user, mqtt_password)
@@ -43,16 +56,41 @@ client.username_pw_set(mqtt_user, mqtt_password)
 client.connect(mqtt_host, mqtt_port, 60)
 
 # Subscribe.
-client.subscribe(topic)
+client.subscribe(output_topic)
 
-rpi_gpio_1 = 5
+
+
+def write_gpio(pin, status):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin, GPIO.OUT)
+    if status == True:
+        GPIO.output(pin, GPIO.HIGH)
+    else:
+        GPIO.output(pin, GPIO.LOW)
+
+# GPIO setup.
+def input_gpio_setup(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    # Declare as input.
+    GPIO.setup(pin, GPIO.IN)
+
+# Declare pin.
+input_pin = 21
+# Call setup.
+input_gpio_setup(input_pin)
 
 # Start loop.
 client.loop_start()
 # Loop.
 while True:
-    # gpiostatus = rpi_gpio_1.read() # Change with function to read pin status
-    # client.publish(topic, gpiostatus)
+    if GPIO.input(input_pin):
+        client.publish(input_topic, 'HIGH')
+    else:
+        client.publish(input_topic, 'LOW')
     time.sleep(10)
+
+# Disconnect.
 client.loop_stop()
 client.disconnect()
